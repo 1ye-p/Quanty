3814480c0aafcc7e05b7f10a85a1a99468fb7ad6
y1ye <rcritical@qq.com>
2026-06-14 14:17:13 +0800

    chore(web): install zustand and create stores directory

---
+++ web/src/stores/index.ts
@@ -0,0 +1,5 @@
+export { useWorkflowStore } from './workflowStore';
+export { useThemeStore } from './themeStore';
+export { useSidebarStore } from './sidebarStore';
+export { useBacktestCompareStore } from './backtestCompareStore';
+export { useFactorExperimentStore } from './factorExperimentStore';
